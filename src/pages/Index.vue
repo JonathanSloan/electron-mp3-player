a929a59144bec266302c40a53bc4949f175eca74
JonathanSloan <jonathanssloan@protonmail.com>
2021-01-29 19:55:53 -0500

    Began tinkering with FS

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,39 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+    <q-btn label="Create File" @click="createFile" />
   </q-page>
 </template>
 
 <script>
+// Filesystem module
+const fs = require("fs");
+
+//
+const { dialog } = require("electron").remote;
+
 export default {
-  name: 'PageIndex'
-}
+  name: "PageIndex",
+
+  methods: {
+    createFile() {
+      let content = "Hello, this is the content of the new file.";
+
+      dialog.showSaveDialog(() => {
+        if (filename === undefined) {
+          console.log("no file name");
+          return;
+        }
+
+        fs.writeFile(filename, content, err => {
+          if (err) {
+            console.log(`error writing file ${err.message}`);
+            return;
+          }
+
+          alert("File successfully created!");
+        });
+      });
+    }
+  }
+};
 </script>
